0645c78d5282bff86c7cb892809fdecad80a99a2
Martin L <martivl@stud.ntnu.no>
2020-09-23 22:51:17 +0200

    add movable player with mouse support and ++

--- js/game.js
+++ js/game.js
@@ -1,35 +1,66 @@
 "use  strict";
 
 
+
 class Game {
   constructor(canvas) {
     this.canvas = canvas; //setting canvas
     this.ctx = this.canvas.getContext("2d"); //setting context
 
-    this.fps = 10; // setting frames per second
+    this.fps = 200; // setting frames per second
     this.running = false; //handy variable for statrting and stopping the render function
-    this.start(); // starting TODO: remove this
-    this.mouse = { //object for finding x and y values of the client mouse relative to canvas
-      x: function() {
-        return window.event.clientX - this.canvas.offsetTop;
-      }
-      y: function() {
-        return window.event.clientY - this.canvas.offsetLeft;
+
+    this.player = { //object for holding the x,y positions of the player
+      x: undefined,
+      y: undefined,
+      update: function(x,y) {
+        this.x = x;
+        this.y = y;
       }
     }
+    this.ball = {
+      x:undefined,
+      y:undefined,
+      dx: undefined,
+      dy: undefined,
+      update: function(canvas,player) {
+
+        this.x += this.dx;
+        this.y += this.dy;
+      }
+    }
+    //add eventListener to update mouse position
+    this.canvas.addEventListener("mousemove",this.updatePlayer.bind(this));
+
+    this.start();
+
   }
-  render() {
+  render() { //functon for drawing each new frame, idk why it is called render
 
-
-    window.setTimeout(function() {
-      t.render();
-    },1000/this.fps);
-
+    //TODO: declare d2d in constructor and make global
+    this.updateBall();
+    const d2d = new drawLib(this.canvas);
+    d2d.clear();
+    d2d.drawRect(this.player.x,this.canvas.height-50,100,25,"red");
   }
   start() {
 
-    let t = this; // storing this variable since we cant refence in the settimoute
-    window.setTimeout(t.render(),1000/this.fps);
-    this.render();
+    this.running = true;
+    this.interval = setInterval( //starts callingthe render function after fps specs
+      //named it this.interval ,which comes in handy if we need to stop it
+      this.render.bind(this),
+      1000/this.fps
+    )
+
+
   }
+  updatePlayer(e) {
+    const cX = e.clientX - this.canvas.offsetLeft; //canvas relative x cordinates
+    const cY = e.clientY - this.canvas.offsetTop;  //canvas relative y cordinates
+    this.player.update(cX,cY);
+  }
+  updateBall() {
+    //this should do something in alter version
+  }
+
 }
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.onload = function() {
   const canvas = document.getElementById("gameCanvas"); // there is only one canvas
   canvas.width = 600;
   canvas.height = 600;
-  
+
 
   //d2d = draw 2d, object that we use for drawing
 
@@ -15,6 +15,9 @@ window.onload = function() {
   //d2d.drawRect(50,50,50,50,"black");
   //let c = canvas.getContext("2d");
   //c.fillRect(0,0,100,100)
+  const g = new Game(canvas);
+
+
 
 
 }
